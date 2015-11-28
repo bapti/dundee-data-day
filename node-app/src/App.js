e0bbc2732aeebad4e4128411eb1241121a3e9ce1
Richie Mackay <richie.mackay@jandltech.com>
2015-11-28 16:00:23 +0000

    updating socket path

--- node-app/src/App.js
+++ node-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 
-let socket = io.connect('http://localhost:5000');
+let socket = io.connect('http://192.168.99.100:5000');
 
 class Counter extends Component {
   constructor(props) {
