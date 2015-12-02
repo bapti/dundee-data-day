4460e08bbe236e51d7a4f48120fd2db383abc729
Neil Crawford <neil.crawford@gmail.com>
2015-12-02 21:55:57 +0000

    Button versions

--- node-app/server.js
+++ node-app/server.js
@@ -1,62 +1,65 @@
-(function () {
-    'use strict';
+var path = require('path'),
+    express = require('express'),
+    webpack = require('webpack'),
+    config = require('./webpack-config'),
+    Prometheus = require("prometheus-client"),
 
-    var path = require('path'),
-        express = require('express'),
-        webpack = require('webpack'),
-        config = require('./webpack-config'),
-        Prometheus = require("prometheus-client"),
+    app = express(),
+    compiler = webpack(config),
+    client = new Prometheus(),
 
-        app = express(),
-        compiler = webpack(config),
-        client = new Prometheus(),
+    counter = client.newCounter({
+      namespace: "dundee_data_day",
+      name: "button_counter",
+      help: "The number of times the button has been clicked"
+    }),
 
-        counter = client.newCounter({
-          namespace: "dundee_data_day",
-          name: "button_counter",
-          help: "The number of times the button has been clicked"
-        }),
-
-        errorCounter = client.newCounter({
-          namespace: "dundee_data_day",
-          name: "error_counter",
-          help: "The number of errors thrown"
-        })
+    errorCounter = client.newCounter({
+      namespace: "dundee_data_day",
+      name: "error_counter",
+      help: "The number of errors thrown"
+    })
 
 
-    app.use(require('webpack-dev-middleware')(compiler, {
-        noInfo: true,
-        publicPath: config.output.publicPath
-    }));
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-    app.get("/metrics", client.metricsFunc());
+app.get("/metrics", client.metricsFunc());
 
-    var server = app.listen(5000, function (err) {
-        if (err) {
-            console.log(err);
-            return;
+var server = app.listen(5000, function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Listening at http://localhost:5000');
+});
+
+var io = require('socket.io')(server);
+
+var count = 0;
+
+io.on('connection', function (socket) {
+    socket.emit('count', { count: count } );
+
+    socket.on('increment', function (data) {
+        count++;
+        console.log("increment count: " + count, data.version);
+        socket.emit('count', {count: count});
+        socket.broadcast.emit('count', {count: count});
+
+        if( data.version == 1 ){
+          counter.increment()
         }
 
-        console.log('Listening at http://localhost:5000');
+        if( data.version == 2 && count % 5 == 0 ) {
+          errorCounter.increment({feature: "counter_button", version: 2})
+        }
     });
-
-    var io = require('socket.io')(server);
-
-    var count = 0;
-
-    io.on('connection', function (socket) {
-        socket.emit('count', {count: count});
-
-        socket.on('increment', function (version) {
-            count++;
-            console.log("increment count: " + count, version);
-            socket.emit('count', {count: count});
-            socket.broadcast.emit('count', {count: count});
-            counter.increment({version: version});
-        });
-    });
-})();
+});
--- node-app/src/App.js
+++ node-app/src/App.js
@@ -4,41 +4,72 @@ import io from 'socket.io-client';
 let socket = io.connect(window.location.href);
 
 class CounterButton extends Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this)
-        this.state = {count: 0};
-        let self = this;
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+    this.state = { count: 0 };
+    let self = this;
 
-        socket.on('count', function (data) {
-            console.log(data);
-            self.setState({count: data.count})
-        })
-    }
+    socket.on('count', function (data) {
+        console.log( data )
+        self.setState( data )
+    })
+  }
 
-    getInitialState() {
-      return { count: 0 };
-    }
+  getInitialState() {
+    return { count: 0 };
+  }
 
-    handleClick(obj) {
-      socket.emit( 'increment', { version: this.props.version } );
-    }
+  handleClick(obj) {
+    socket.emit( 'increment', { version: this.props.version } );
+  }
 
-    render() {
-      return (
-        <button onClick={this.handleClick} style={{ color: this.props.color }}>
-          Count: {this.state.count}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button onClick={this.handleClick} style={{ color: this.props.color }}>
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+
+class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    self = this;
+    socket.on('disable_feature', function({feature, version}){
+      if(this.props.feature === feature && this.props.version === version){
+        self.setState({ enabled: false })
+      }
+    })
+  }
+
+  getInitialState() {
+    return { enabled: this.props.enabled };
+  }
+
+  render() {
+    return (
+      <div style={{ display: this.props.enabled ? 'inline' : 'none' }}>
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <CounterButton color={'red'} version='1' />
-        <CounterButton color={'green'} version='2' />
+
+        <Toggler feature="counter_button" version="1" enabled={true}>
+          <CounterButton color={'red'} version='1' />
+        </Toggler>
+
+        <Toggler  feature="counter_button" version="2" enabled={false}>
+          <CounterButton color={'green'} version='2' />
+        </Toggler>
+
       </div>
     );
   }
