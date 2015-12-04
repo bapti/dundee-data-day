b60f961bca254cbf8353bf026d350ed771c0cd59
Richie Mackay <richie.mackay@jandltech.com>
2015-12-04 13:30:29 +0000

    fix data

--- node-app/server.js
+++ node-app/server.js
@@ -37,7 +37,7 @@ app.get("/metrics", client.metricsFunc());
 
 app.post("/feature", jsonParser, function(request, response){
     console.log("HITTING disable_feature", request.body);
-    io.emit("disable_feature", request.body.alert[0].payload);
+    io.emit("disable_feature", request.body);
     response.write(request.body);
     response.end();
 })
